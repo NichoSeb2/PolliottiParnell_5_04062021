2058e74f0c887b1b163f98f1bafc38ea36d22454
NichoSeb2 <NichoSeb2@outlook.fr>
2021-06-15 10:54:49 +0200

    added basic router

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+define('ROOT_DIR', realpath(dirname(__DIR__)));
+define('CONF_DIR', ROOT_DIR. '/config');
+define('TEMPLATE_DIR', ROOT_DIR. '/templates');
+
+require_once(ROOT_DIR. '/vendor/autoload.php');
+
+use App\Core\Router;
+
+try {
+	$router = new Router();
+
+	$controller = $router->getController();
+
+	if (!is_null($controller)) $controller->execute();
+	else {
+		echo("404");
+	}
+} catch (\Exception $e) {
+	echo("Router initialization failed");
+}
