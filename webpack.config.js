6d0d479b817a234ceadf3ba7ee04a54451b17b3b
NichoSeb2 <NichoSeb2@outlook.fr>
2021-06-19 15:57:19 +0200

    added webpack, added basic sass structure

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+
+module.exports = {
+	context: path.resolve(__dirname, "public/scss"),
+	entry: {
+		main: ["./main.scss"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
+		],
+	},
+	plugins: [
+		new RemoveEmptyScriptsPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].min.css",
+		}),
+	],
+	output: {
+		path: path.resolve(__dirname, "public/css"),
+	},
+};
